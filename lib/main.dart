1a8207c4f5d4bbe830e7398e35afc18db618e7c7
Sydney D. Abuyabor <sydneyabuyabor@gmail.com>
2024-03-09 21:32:40 +0800

    Added Login Forms

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -66,6 +67,112 @@ class _LoginPageState extends State<LoginPage> {
                         image: AssetImage("assets/images/clock.png"),
                       ),
                     ),
+                  ),
+                ),
+                Positioned(
+                  child: Container(
+                    margin: EdgeInsets.all(50),
+                    child: const Center(
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                            fontSize: 40,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(0),
+            child: Column(
+              children: [
+                Text(
+                  "LOGIN",
+                  style: TextStyle(fontSize: 40, color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromRGBO(143, 148, 251, .2),
+                        blurRadius: 20.0,
+                        offset: Offset(0, 10),
+                      ),
+                    ],
+                  ),
+                  child: Column(children: [
+                    Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(color: Colors.grey.shade100),
+                        ),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Email or Phone number",
+                          hintStyle: TextStyle(color: Colors.grey.shade400),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Password",
+                          hintStyle: TextStyle(color: Colors.grey.shade400),
+                        ),
+                      ),
+                    ),
+                  ]),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromRGBO(143, 148, 251, 1),
+                        Color.fromRGBO(143, 148, 251, .6),
+                      ],
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Text(
+                  "Forgot Password?",
+                  style: TextStyle(
+                    color: Color.fromRGBO(143, 148, 251, 1),
                   ),
                 ),
               ],
