3e7be1ff7ef4d2547144305e14b9e3b894569abb
Sydney D. Abuyabor <sydneyabuyabor@gmail.com>
2024-03-10 00:17:18 +0800

    Implement fade animation of login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:login_page/Animation/FadeAnimation.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -37,10 +38,13 @@ class _LoginPageState extends State<LoginPage> {
                     left: 30,
                     width: 80,
                     height: 200,
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage("assets/images/light-1.png"),
+                    child: FadeAnimation(
+                      0.5,
+                      Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("assets/images/light-1.png"),
+                          ),
                         ),
                       ),
                     ),
@@ -49,10 +53,13 @@ class _LoginPageState extends State<LoginPage> {
                     left: 140,
                     width: 80,
                     height: 150,
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage("assets/images/light-1.png"),
+                    child: FadeAnimation(
+                      0.3,
+                      Container(
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("assets/images/light-2.png"),
+                          ),
                         ),
                       ),
                     ),
@@ -62,38 +69,33 @@ class _LoginPageState extends State<LoginPage> {
                     top: 40,
                     width: 80,
                     height: 150,
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage("assets/images/clock.png"),
+                    child: FadeAnimation(
+                      0.6,
+                      Container(
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("assets/images/clock.png"),
+                          ),
                         ),
                       ),
                     ),
                   ),
                   Positioned(
-                    child: Container(
-                      margin: EdgeInsets.all(50),
-                      child: const Center(
-                        child: Text(
-                          "Login",
-                          style: TextStyle(
-                              fontSize: 40,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold),
+                    child: FadeAnimation(
+                      1,
+                      Container(
+                        margin: EdgeInsets.all(50),
+                        child: const Center(
+                          child: Text(
+                            "Login",
+                            style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(0),
-              child: Column(
-                children: [
-                  Text(
-                    "LOGIN",
-                    style: TextStyle(fontSize: 40, color: Colors.white),
                   ),
                 ],
               ),
@@ -102,78 +104,87 @@ class _LoginPageState extends State<LoginPage> {
               padding: EdgeInsets.all(30),
               child: Column(
                 children: [
-                  Container(
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color.fromRGBO(143, 148, 251, .2),
-                          blurRadius: 20.0,
-                          offset: Offset(0, 10),
+                  FadeAnimation(
+                    0.7,
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color.fromRGBO(143, 148, 251, .2),
+                            blurRadius: 20.0,
+                            offset: Offset(0, 10),
+                          ),
+                        ],
+                      ),
+                      child: Column(children: [
+                        Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(color: Colors.grey.shade100),
+                            ),
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Email or Phone number",
+                              hintStyle: TextStyle(color: Colors.grey.shade400),
+                            ),
+                          ),
                         ),
-                      ],
+                        Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Password",
+                              hintStyle: TextStyle(color: Colors.grey.shade400),
+                            ),
+                          ),
+                        ),
+                      ]),
                     ),
-                    child: Column(children: [
-                      Container(
-                        padding: EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          border: Border(
-                            bottom: BorderSide(color: Colors.grey.shade100),
-                          ),
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Email or Phone number",
-                            hintStyle: TextStyle(color: Colors.grey.shade400),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(8),
-                        decoration: BoxDecoration(),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Password",
-                            hintStyle: TextStyle(color: Colors.grey.shade400),
-                          ),
-                        ),
-                      ),
-                    ]),
                   ),
                   SizedBox(
                     height: 30,
                   ),
-                  Container(
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      gradient: LinearGradient(
-                        colors: [
-                          Color.fromRGBO(143, 148, 251, 1),
-                          Color.fromRGBO(143, 148, 251, .6),
-                        ],
+                  FadeAnimation(
+                    0.5,
+                    Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        gradient: LinearGradient(
+                          colors: [
+                            Color.fromRGBO(143, 148, 251, 1),
+                            Color.fromRGBO(143, 148, 251, .6),
+                          ],
+                        ),
                       ),
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold),
+                      child: Center(
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
                     ),
                   ),
                   SizedBox(
                     height: 70,
                   ),
-                  Text(
-                    "Forgot Password?",
-                    style: TextStyle(
-                      color: Color.fromRGBO(143, 148, 251, 1),
+                  FadeAnimation(
+                    0.5,
+                    Text(
+                      "Forgot Password?",
+                      style: TextStyle(
+                        color: Color.fromRGBO(143, 148, 251, 1),
+                      ),
                     ),
                   ),
                 ],
