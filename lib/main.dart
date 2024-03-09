0e3e0299ed1a34d66c76527b0ff1750a839c2ade
Sydney D. Abuyabor <sydneyabuyabor@gmail.com>
2024-03-09 14:08:24 +0800

    Background for loginPage"

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,62 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Container(
+            height: 400,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/images/background.png'),
+              ),
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 30,
+                  width: 80,
+                  height: 200,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/light-1.png"),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 140,
+                  width: 80,
+                  height: 150,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/light-1.png"),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 40,
+                  top: 40,
+                  width: 80,
+                  height: 150,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/clock.png"),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
